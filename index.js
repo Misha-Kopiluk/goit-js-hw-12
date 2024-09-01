4027b613bbbc104c3d8bc9382061dbee70c51684
Misha-Kopiluk <164411176+Misha-Kopiluk@users.noreply.github.com>
2024-09-01 13:39:59 +0000

    Deploying to gh-pages from @ Misha-Kopiluk/goit-js-hw-12@1d2f833e401098ac6292cab427da433d4dab1423 🚀

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-import{a as b,S as v,i}from"./assets/vendor-CcU07tmu.js";(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))o(e);new MutationObserver(e=>{for(const s of e)if(s.type==="childList")for(const c of s.addedNodes)c.tagName==="LINK"&&c.rel==="modulepreload"&&o(c)}).observe(document,{childList:!0,subtree:!0});function a(e){const s={};return e.integrity&&(s.integrity=e.integrity),e.referrerPolicy&&(s.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?s.credentials="include":e.crossOrigin==="anonymous"?s.credentials="omit":s.credentials="same-origin",s}function o(e){if(e.ep)return;e.ep=!0;const s=a(e);fetch(e.href,s)}})();const w="https://pixabay.com/api/",x="45654098-015d10b92a1455385bf1e768f",g=(t,r)=>{const a={params:{q:t,orientation:"horizontal",image_type:"photo",safesearch:"true",per_page:15,page:r}};return b.get(`${w}?key=${x}`,a)},h=t=>`<li class="gallery-item">
-    <a class="gallery-link" href="${t.largeImageURL}">
-    <img class="gallery-image" src="${t.webformatURL}" alt="${t.tags}" />
+import{a as b,S as v,i}from"./assets/vendor-CcU07tmu.js";(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const t of document.querySelectorAll('link[rel="modulepreload"]'))a(t);new MutationObserver(t=>{for(const s of t)if(s.type==="childList")for(const c of s.addedNodes)c.tagName==="LINK"&&c.rel==="modulepreload"&&a(c)}).observe(document,{childList:!0,subtree:!0});function o(t){const s={};return t.integrity&&(s.integrity=t.integrity),t.referrerPolicy&&(s.referrerPolicy=t.referrerPolicy),t.crossOrigin==="use-credentials"?s.credentials="include":t.crossOrigin==="anonymous"?s.credentials="omit":s.credentials="same-origin",s}function a(t){if(t.ep)return;t.ep=!0;const s=o(t);fetch(t.href,s)}})();const w="https://pixabay.com/api/",x="45654098-015d10b92a1455385bf1e768f",h=(e,r)=>{const o={params:{q:e,orientation:"horizontal",image_type:"photo",safesearch:"true",per_page:15,page:r}};return b.get(`${w}?key=${x}`,o)},y=e=>`<li class="gallery-item">
+    <a class="gallery-link" href="${e.largeImageURL}">
+    <img class="gallery-image" src="${e.webformatURL}" alt="${e.tags}" />
     </a>
     <div class="wrapper">
     <ul class="img">
       <li class="text-item">
-        Likes<span class="text">${t.likes}</span>
+        Likes<span class="text">${e.likes}</span>
       </li>
       <li class="text-item">
-        Views<span class="text">${t.views}</span>
+        Views<span class="text">${e.views}</span>
       </li>
       <li class="text-item">
-        Comments<span class="text">${t.comments}</span>
+        Comments<span class="text">${e.comments}</span>
       </li>
       <li class="text-item">
-        Downloads<span class="text">${t.downloads}</span>
+        Downloads<span class="text">${e.downloads}</span>
       </li>
     </ul>
   </div>
-  </li>`,d=document.querySelector(".forms"),m=document.querySelector(".gallery"),y=document.querySelector(".js-loader"),p=document.querySelector(".js-load-more-btn"),f=new v(".gallery a",{captionsData:"alt",captionDelay:250,overlayOpacity:.8});let n=1,l="",L=0;const R=()=>{y.classList.remove("hidden")},u=()=>{y.classList.add("hidden")},q=async t=>{R();try{if(t.preventDefault(),l=d.elements.user.value.trim(),n=1,l===""){i.warning({position:"topRight",message:"Fill in the input field"}),u();return}const r=await g(l,n);if(r.data.hits.length===0){i.error({position:"topRight",message:"Sorry, there are no images matching your search query. Please try again!"}),m.innerHTML="";return}const a=r.data.hits.map(e=>h(e)).join("");m.innerHTML=a,f.refresh(),L=document.querySelector(".gallery-item").getBoundingClientRect().height,p.classList.remove("is-hidden")}catch{i.error({position:"topRight",message:"An error occurred"})}finally{u()}d.reset()},S=async()=>{try{n++;const t=await g(l,n),r=t.data.hits.map(o=>h(o)).join("");m.insertAdjacentHTML("beforeend",r),f.refresh(),window.scrollBy({top:L*2,behavior:"smooth"});const a=Math.ceil(t.data.totalHits/15);n>=a&&(i.info({position:"topRight",message:"We're sorry, but you've reached the end of search results."}),p.classList.add("is-hidden"))}catch{i.error({position:"topRight",message:"An error occurred"})}};d.addEventListener("submit",q);p.addEventListener("click",S);
+  </li>`,d=document.querySelector(".forms"),m=document.querySelector(".gallery"),f=document.querySelector(".js-loader"),p=document.querySelector(".js-load-more-btn"),L=new v(".gallery a",{captionsData:"alt",captionDelay:250,overlayOpacity:.8});let n=1,l="",u=0;const R=()=>{f.classList.remove("hidden")},g=()=>{f.classList.add("hidden")},S=async e=>{R();try{if(e.preventDefault(),l=d.elements.user.value.trim(),n=1,l===""){i.warning({position:"topRight",message:"Fill in the input field"}),g();return}const r=await h(l,n);if(r.data.hits.length===0){i.error({position:"topRight",message:"Sorry, there are no images matching your search query. Please try again!"}),m.innerHTML="";return}const o=r.data.hits.map(a=>y(a)).join("");m.innerHTML=o,L.refresh(),p.classList.remove("is-hidden")}catch{i.error({position:"topRight",message:"An error occurred"})}finally{g()}d.reset()},q=()=>{u=document.querySelector(".gallery-item").getBoundingClientRect().height,window.scrollBy({top:u*2,behavior:"smooth"})},M=async()=>{try{n++;const e=await h(l,n),r=e.data.hits.map(a=>y(a)).join("");m.insertAdjacentHTML("beforeend",r),L.refresh(),q();const o=Math.ceil(e.data.totalHits/15);n>=o&&(i.info({position:"topRight",message:"We're sorry, but you've reached the end of search results."}),p.classList.add("is-hidden"))}catch{i.error({position:"topRight",message:"An error occurred"})}};d.addEventListener("submit",S);p.addEventListener("click",M);
 //# sourceMappingURL=index.js.map
